7395c9cf57d9c4ce35fe7cf42a1e5d84a08210b9
Roberto Bezerra <90281274+robertobzrr@users.noreply.github.com>
2025-03-24 14:46:30 -0300

    Update Leao.java

--- src/Leao.java
+++ src/Leao.java
@@ -1,8 +1,22 @@
-public class Leao {
+package primeiraQuestao;
 
-            boolean juba;
+public class Leao extends Animal {
+    private boolean juba;
+
+    public boolean getJuba() {
+        return juba;
+    }
 
     public void setJuba(boolean juba) {
         this.juba = juba;
+    }
+
+    public void cacar() {
+        System.out.println("estou cacando");
+    }
+
+    @Override
+    public void comer() {
+        System.out.println("Nham nahm! que cavalo gostoso");
     }
 }
