9983754e978957acc887a7cab62ba179110f67bc
Roberto Bezerra <90281274+robertobzrr@users.noreply.github.com>
2025-03-24 14:45:36 -0300

    Update Animal.java

--- src/Animal.java
+++ src/Animal.java
@@ -1,12 +1,27 @@
+package primeiraQuestao;
+
 public class Animal {
+    protected float tamanho;
+    protected String cor;
 
-            float tamanho;
-            String cor;
+    public void setTamanho(float tamanho){
+        this.tamanho = tamanho;
+    }
 
+    public float getTamanho(){
+        return this.tamanho;
+    }
 
-        public void comer(){
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
 
-        }
+    public String getCor(){
+        return cor;
+    }
 
+    public void comer(){
+        System.out.println("Nham nahm! que matinho gostoso");
+    }
 
 }
